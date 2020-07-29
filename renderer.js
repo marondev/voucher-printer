71a4468da85b34bb23d226c3bfc05f85ebba7ce7
Maron Aquillo <maronaquillo@gmail.com>
2020-07-30 01:36:49 +0800

    initial commit

--- renderer.js
+++ renderer.js
@@ -1,3 +1,62 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const escpos = require('escpos');
+const fs = require('fs');
+
+const printCode = function( codes = [] ) {
+
+    const usbDevice = new escpos.USB();
+    const usbPrinter = new escpos.Printer(usbDevice);
+    
+    usbDevice.open(() => {
+        codes.forEach( code => {
+
+            var title = '';
+            var row = code
+                .trim()
+                .replace(/\s/g, '\t')
+                .split('\t');
+            
+
+            if ( row[2] ) {
+                title = `Time: ${row[0]}     Price: ${row[2]}`;
+                code = row[1];
+            } else if( row[1] ) {
+                title = `Time: ${row[0]}`;
+                code = row[1];
+            }
+
+            usbPrinter
+                .font('A')
+                .align('ct')
+                .style('bu')
+                .size(1,1)
+                .text('------------------------------\n')
+                .size(2,2)
+                .text(code + '\n');
+            
+            if ( title ) {
+                usbPrinter
+                    .size(1,1)
+                    .text(title);
+            }
+           
+        });
+
+        usbPrinter.cut();
+        usbPrinter.close();
+    });
+}
+
+Dropzone.options.voucher = {
+    init: function() {
+        this.on("addedfile", function(file) {
+            try {
+                var data = fs.readFileSync(file.path, 'utf8');
+                var codes = data.split('\n');
+                printCode( codes );
+
+            } catch(e) {
+                console.log('Error:', e.stack);
+            }
+        });
+    },
+};
